9c41bb405ce22b5a76cc17af67fbca2aabd3671d
maarlesh <subamaarleshbaskar@gmail.com>
2022-08-10 18:09:47 +0530

    Add project files.

---
+++ ContactsApp/AddContactWIndow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Interaction logic for AddContactWIndow.xaml
+    /// </summary>
+    public partial class AddContactWIndow : Window
+    {
+        public AddContactWIndow()
+        {
+            InitializeComponent();
+        }
+    }
+}
